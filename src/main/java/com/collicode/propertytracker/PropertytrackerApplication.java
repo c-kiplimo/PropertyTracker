87fcb970b1c08007856fab73d9d8a948dc1b273b
c-kiplimo <kiplimocollins855@gmail.com>
2024-06-03 01:39:06 +0300

    initial commit

---
+++ src/main/java/com/collicode/propertytracker/PropertytrackerApplication.java
@@ -0,0 +1,13 @@
+package com.collicode.propertytracker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PropertytrackerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PropertytrackerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/collicode/propertytracker/PropertytrackerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.collicode.propertytracker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PropertytrackerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
