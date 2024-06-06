6b8c45da46b3887bd52f0bf2dbd81fec778c6b42
Collins48 <collinkimutai648@gmail.com>
2024-06-06 18:44:53 +0300

    room

--- src/main/java/com/collicode/propertytracker/infrastructure/projections/Room.java
+++ src/main/java/com/collicode/propertytracker/infrastructure/projections/Room.java
@@ -1,5 +1,24 @@
 package com.collicode.propertytracker.infrastructure.projections;
 
-public class Room {
+import jakarta.persistence.*;
 
+import java.util.List;
+
+@Entity
+public class Room {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String address;
+    private String city;
+    private String state;
+    private String zipCode;
+    private double price;
+
+    @OneToMany(mappedBy = "property", cascade = CascadeType.ALL)
+    private List<Room> rooms;
+
+    // Getters and setters
 }
+
+
