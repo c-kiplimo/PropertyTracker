f4da5ee6978a6cf1c4fcb526bf33bfea1e2d48a5
Collins48 <collinkimutai648@gmail.com>
2024-06-06 16:52:03 +0300

    updated room

--- src/main/java/com/collicode/propertytracker/infrastructure/projections/Room.java
+++ src/main/java/com/collicode/propertytracker/infrastructure/projections/Room.java
@@ -1,5 +1,22 @@
 package com.collicode.propertytracker.infrastructure.projections;
 
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+@Entity
 public class Room {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String roomNumber;
+    private String type;
+    private double price;
+    private boolean available;
+
+    // Getters and setters
 
 }
