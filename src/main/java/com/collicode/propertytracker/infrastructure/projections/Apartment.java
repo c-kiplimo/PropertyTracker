198b017cc19ed432960f462aa240b4178405bf7e
Collins48 <collinkimutai648@gmail.com>
2024-06-07 12:35:36 +0300

    add properties to apartment module

--- src/main/java/com/collicode/propertytracker/infrastructure/projections/Apartment.java
+++ src/main/java/com/collicode/propertytracker/infrastructure/projections/Apartment.java
@@ -1,4 +1,29 @@
 package com.collicode.propertytracker.infrastructure.projections;
 
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.time.LocalDateTime;
+
+@Entity
+@Table (name = "tbl_apartment")
+@Getter
+@Setter
 public class Apartment {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long apartmentId;
+    private long agentCode;
+    private String apartmentName;
+    private String address;
+    private String location;
+    private LocalDateTime createdAt;
+    private LocalDateTime updatedAt;
+    private String auditInfo;
+
+
+
 }
+
+
