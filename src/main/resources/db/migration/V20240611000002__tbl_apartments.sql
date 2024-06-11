6227775a90c156d899b46ff383ac2092514c69c2
c-kiplimo <kiplimocollins855@gmail.com>
2024-06-11 16:27:54 +0300

    add table  appartment

--- src/main/resources/db/migration/V20240611000002__tbl_apartments.sql
+++ src/main/resources/db/migration/V20240611000002__tbl_apartments.sql
@@ -1,6 +1,6 @@
 CREATE TABLE IF NOT EXISTS tbl_apartment
 (
-    aparment_id      SERIAL,
+    apartment_id      SERIAL,
     apartment_name    varchar(255) NOT NULL,
     agent_code bigint       NOT NULL,
     address varchar(255)    NOT NULL,
@@ -8,5 +8,5 @@ CREATE TABLE IF NOT EXISTS tbl_apartment
     created_at   timestamp    NOT NULL,
     updated_at   timestamp    NULL,
     audit_info   text DEFAULT '{}',
-    PRIMARY KEY (aparment_id)
+    PRIMARY KEY (apartment_id)
 );
