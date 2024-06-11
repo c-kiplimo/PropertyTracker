0f757c5c631e38a8b5ced535b0d83ed5a9c4621e
c-kiplimo <kiplimocollins855@gmail.com>
2024-06-11 15:56:37 +0300

    update table columns

--- src/main/resources/db/migration/V20240611000001__tbl_rooms.sql
+++ src/main/resources/db/migration/V20240611000001__tbl_rooms.sql
@@ -1,9 +1,9 @@
 CREATE TABLE IF NOT EXISTS tbl_room (
-    id SERIAL,
-    address varchar(255) NULL,
-    city varchar(255)  NULL,
-    state varchar(255)  NULL,
-    zip_code varchar(100)  NULL,
-    price numeric NOT NULL,
-    PRIMARY KEY (id)
+    room_id SERIAL,
+    string varchar(255) NOT NULL,
+    apartment_id bigint NOT NULL,
+    created_at timestamp NOT NULL,
+    updated_at timestamp NULL,
+    audit_info timestamp DEFAULT '{}',
+    PRIMARY KEY (room_id)
 );
